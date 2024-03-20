af10321d10ad08334672f70c2f28837b414fe3d7
ZouzouCode <2675820400@qq.com>
2024-03-20 22:11:09 +0800

    file fix
    优化文件路径和说明

--- lineviewtest.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-
-win=tkinter.Tk()
-win.title("test")
-win.geometry("300x300")
-cav=tkinter.Canvas(win,width=300,height=300,bg="white")
-cav.create_oval(50,50,100,100,fill="white",outline="#e83849")
-cav.pack()
-win.mainloop()
